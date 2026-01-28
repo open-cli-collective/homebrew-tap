c7055e51cb5e9084705174ae177f2b7e5d3e7a5c
piekstra <piekstra@users.noreply.github.com>
2026-01-28 12:24:15 -0500

    Add jtk and slck short-name casks (#9)
    ## Summary
    - Add `jtk.rb` cask as a short-name alias for jira-ticket-cli
    - Add `slck.rb` cask as a short-name alias for slack-chat-cli
    - Allows users to install via `brew install
    open-cli-collective/homebrew-tap/jtk` and `brew install
    open-cli-collective/homebrew-tap/slck`
    
    This is consistent with how `cfl` (confluence-cli) already works.
    
    ## Test plan
    - [x] `brew install open-cli-collective/homebrew-tap/jtk` installs
    successfully
    - [x] `jtk --version` works
    - [x] `brew install open-cli-collective/homebrew-tap/slck` installs
    successfully
    - [ ] `slck --version` works
    
    Fixes #6
    Fixes #7

---
+++ Casks/slck.rb
@@ -0,0 +1,44 @@
+cask "slck" do
+  name "slck"
+  desc "Command-line interface for Slack"
+  homepage "https://github.com/open-cli-collective/slack-chat-api"
+  version "3.1.20"
+
+  binary "slck"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/open-cli-collective/slack-chat-api/releases/download/v#{version}/slck_v#{version}_darwin_arm64.tar.gz"
+      sha256 "3c788e63092bca923fc761929d84bba043ed8a17f21ebea6435bdb796a7652de"
+    end
+    on_intel do
+      url "https://github.com/open-cli-collective/slack-chat-api/releases/download/v#{version}/slck_v#{version}_darwin_amd64.tar.gz"
+      sha256 "95ec731163eafd129447cb6865caeed9af9ef1d648cc52c18ebdb4d5ca9a37ef"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/open-cli-collective/slack-chat-api/releases/download/v#{version}/slck_v#{version}_linux_arm64.tar.gz"
+      sha256 "da192f409252c6b03bf5213f37a647ce50d66dcd56d9284d634abbf5583c4afe"
+    end
+    on_intel do
+      url "https://github.com/open-cli-collective/slack-chat-api/releases/download/v#{version}/slck_v#{version}_linux_amd64.tar.gz"
+      sha256 "2881a65a990ef78a49aace02273486c70966368c43e7687e9c48423dbf36eae6"
+    end
+  end
+
+  postflight do
+    system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{staged_path}/slck"]
+  end
+
+  caveats <<~EOS
+    slck has been installed.
+
+    To configure, run:
+      slck config set-token
+
+    On macOS, your token is stored securely in the system Keychain.
+    On Linux, your token is stored in ~/.config/slack-chat-api/credentials.
+  EOS
+end
