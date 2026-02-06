f7f4bd5b90c53872a074eae08b5b39164c200731
piekstra <piekstra@users.noreply.github.com>
2026-02-06 12:55:04 -0500

    Deprecate jtk cask: point to atlassian-cli v0.2.0 with migration notice

--- Casks/jtk.rb
+++ Casks/jtk.rb
@@ -1,30 +1,30 @@
 cask "jtk" do
   name "jtk"
-  desc "Command-line interface for Jira Cloud"
-  homepage "https://github.com/open-cli-collective/jira-ticket-cli"
-  version "0.1.20"
+  desc "[DEPRECATED: use 'brew install jira-ticket-cli' instead] Command-line interface for Jira Cloud"
+  homepage "https://github.com/open-cli-collective/atlassian-cli"
+  version "0.2.0"
 
   binary "jtk"
 
   on_macos do
     on_intel do
-      url "https://github.com/open-cli-collective/jira-ticket-cli/releases/download/v#{version}/jtk_#{version}_darwin_amd64.tar.gz"
-      sha256 "5c9c6ea728eb3da4cda69affbcbeef0e79aeb7686e1d280be4662663cf953b5c"
+      url "https://github.com/open-cli-collective/atlassian-cli/releases/download/v#{version}/jtk_#{version}_darwin_amd64.tar.gz"
+      sha256 "fd4d91cefb28dd53ec301616ffc7eb5822ef98ea030536588e22368db0aa8c5a"
     end
     on_arm do
-      url "https://github.com/open-cli-collective/jira-ticket-cli/releases/download/v#{version}/jtk_#{version}_darwin_arm64.tar.gz"
-      sha256 "623bdaa20b9c6594f2a982c4e68d0648ed9396644f780b0b0730559e541c02fd"
+      url "https://github.com/open-cli-collective/atlassian-cli/releases/download/v#{version}/jtk_#{version}_darwin_arm64.tar.gz"
+      sha256 "8e1fcff1600696fa2c8cd024a4264bd7af0b97567a69c8b18fd5015c327085d9"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/open-cli-collective/jira-ticket-cli/releases/download/v#{version}/jtk_#{version}_linux_amd64.tar.gz"
-      sha256 "ff28d2a45d4c63d6a955a4e8248a2b6230f8b3a769b3fef22c80016dcc73162d"
+      url "https://github.com/open-cli-collective/atlassian-cli/releases/download/v#{version}/jtk_#{version}_linux_amd64.tar.gz"
+      sha256 "d45102227ba369086ca4e618c4a35b593177acf98b065e7c846f90f9c1daac5e"
     end
     on_arm do
-      url "https://github.com/open-cli-collective/jira-ticket-cli/releases/download/v#{version}/jtk_#{version}_linux_arm64.tar.gz"
-      sha256 "d83173304e1712e7f4442af377bc48e1d868b40e39bf865e4318b2c572283357"
+      url "https://github.com/open-cli-collective/atlassian-cli/releases/download/v#{version}/jtk_#{version}_linux_arm64.tar.gz"
+      sha256 "f06141c1289026c27b8e166ddb3817702f3e6dda43b6f4cdf287afe4f6174816"
     end
   end
 
@@ -33,11 +33,11 @@ cask "jtk" do
   end
 
   caveats <<~EOS
-    jtk has been installed.
+    The 'jtk' cask is deprecated. Please switch to 'jira-ticket-cli':
 
-    To configure, run:
-      jtk config set --domain DOMAIN --email EMAIL --token TOKEN
+      brew uninstall jtk
+      brew install open-cli-collective/tap/jira-ticket-cli
 
-    Get your API token from: https://id.atlassian.com/manage-profile/security/api-tokens
+    This cask will continue to receive updates but may be removed in the future.
   EOS
 end
